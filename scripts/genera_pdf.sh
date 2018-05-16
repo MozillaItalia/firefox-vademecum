27c02387ac06a9a880263471e2748cc7937e57f3
Sav22999 <saverio.morelli@outlook.it>
2018-05-16 19:51:19 +0200

    aggiunte variabili "name" e "version"
    name="Vademecum"
    version="2.0"
    per rendere il progetto più "generico" ed "adattabile" possibile sono state implementate le variabili "name" che sarebbe in nome vero e proprio del progetto, in caso si voglia modificare il nome in futuro.
    La variabile versione conterrà solamente la major number (e, in caso, la minor number).
    Inoltre è stata aggiunta anche il TIPO di versione, può essere VG o VT (versione generica o versione tecnica)

--- scripts/genera_pdf.sh
+++ scripts/genera_pdf.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/env bash
 #author: Simone Massaro (mone27)
 #date: 15/05/18
+#last update: 16/15/18 || by: @Sav22999
 #this scripts takes html file converts to single pdf pages and then merges the pages
 #and try to install all dependencies (tested on ubuntu)
 #now paths are hardcoded for firefox-vademecum repository
+#UPDATE the "version" and "type_version" variables and, if necessary, the "name" variable
+name="Vademecum"
+version="2.0"
+type_version="VG"
+#type_version={"VG"|"VT"}
 if ! [ -x "$(command -v html-pdf)" ]; then
-  echo 'html-pdf not installed installing..'
+  echo "html-pdf not installed. installing..."
   if ! [ -x "$(command -v npm)" ]; then
-    echo "npm not installed. Installing..."
+    echo "npm not installed. installing..."
     curl https://www.npmjs.org/install.sh | sh
   fi
   npm install html-pdf
 fi
 html-pdf ../html/volantino_fronte.html ../volantino/volantino_fronte.pdf
 html-pdf ../html/volantino_retro.html ../volantino/volantino_retro.pdf
-echo "generated pdf files. merging.."
+echo "files pdf generated. merging in a single file..."
 
 if ! [ -x "$(command -v pdftk)" ]; then
   if ! [ -x "$(command -v apt)" ]; then
-    echo "platform non supported install pdftk for your distribution the try again"
+    echo "platform doesn't support the installation pdftk for your distribution. so, try again."
   fi
   sudo apt install pdftk
 fi
 cd ../volantino
-pdftk volantino_fronte.pdf volantino_retro.pdf cat output "Vademecum-$(date +"%Y_%m_%d_%I_%M").pdf"
+pdftk volantino_fronte.pdf volantino_retro.pdf cat output "${name}_${version}_${type_version}-$(date +"%Y_%m_%d_%I_%M").pdf"

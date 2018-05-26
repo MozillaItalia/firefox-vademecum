f1da68de6d3e21b440dee40757c26569811db3db
Sav22999 <saverio.morelli@outlook.it>
2018-05-26 18:42:53 +0200

    Aumentata l'alteza di 0.1cm e aggiornato lo script
    Aggiornamento script - Dettagli:
    - Conversione HTML->PDF, poi merge in un unico pdf
    - Conversione HTML->PNG
    
    è necessario specificare la versione e il tipo di versione. Questi dati possono, in caso, essere fissati.
    
    - Rimossi i credits di simone: i nomi sono presenti già a fine vademecum nella sezione "Collaboratori"
    - Sostituito pdftk con pdfunite
    - Rimossi i margini nel pdf correttamente
    - Immagine tagliata correttamente

--- scripts/genera_pdf.sh
+++ scripts/genera_pdf.sh
@@ -1,36 +1,37 @@
-#!/usr/bin/env bash
-#author: Simone Massaro (mone27)
-#date: 15/05/18
-#last update: 16/15/18 || by: @Sav22999
-#this scripts takes html file converts to single pdf pages and then merges the pages
-#and try to install all dependencies (tested on ubuntu)
-#now paths are hardcoded for firefox-vademecum repository
-#UPDATE, if it's necessary, the "name" variable
+# this scripts takes html file converts to single pdf pages and then merges the pages
+# and try to install all dependencies (tested on ubuntu and kubuntu)
+# now paths are hardcoded for firefox-vademecum repository
+# update manually, if it's necessary, the "name" variable
+
 name="Vademecum"
 echo "Version number (format: VV.VV): "
 read version
 echo "Version type {VG|VT}: "
 read typeVersion
-#type_version={"VG"|"VT"}
-if ! [ -x "$(command -v html-pdf)" ]; then
-  echo "html-pdf not installed. installing..."
-  if ! [ -x "$(command -v npm)" ]; then
-    echo "npm not installed. installing..."
-    curl https://www.npmjs.org/install.sh | sh
-  fi
-  npm install html-pdf
-fi
-html-pdf ../html/volantino_fronte.html ../volantino/volantino_fronte.pdf
-html-pdf ../html/volantino_retro.html ../volantino/volantino_retro.pdf
-echo "pdf files generated. merging in a single file..."
 
+if ! [ -x "$(command -v wkhtmltopdf)" ]; then
+  sudo apt install wkhtmltopdf
+fi
+echo "pdf files generating..."
+wkhtmltopdf -B 0 -T 0 -L 0 -R 0 ../html/volantino_fronte.html ../volantino/"$name"' '"$typeVersion"' FRONTE.pdf'
+wkhtmltopdf -B 0 -T 0 -L 0 -R 0 ../html/volantino_retro.html ../volantino/"$name"' '"$typeVersion"' RETRO.pdf'
+echo "pdf files generated."
+
+echo "png file generating..."
+wkhtmltoimage --crop-w 992 ../html/volantino_fronte.html ../volantino/"$name"' '"$typeVersion"' 'FRONTE.png
+wkhtmltoimage --crop-w 992 ../html/volantino_retro.html ../volantino/"$name"' '"$typeVersion"' 'RETRO.png
+echo "png files generated."
+
+echo "pdf files merging in a single pdf file..."
 if ! [ -x "$(command -v pdftk)" ]; then
   if ! [ -x "$(command -v apt)" ]; then
     echo "The script does not support automatic installation of pdftk on your platform."\
-	"Please install pdftk (https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/) then try again"
+	"Please install pdfunite (poppler-utils) and then try again."
 	exit 1
   fi
-  sudo apt install pdftk
+  sudo apt install poppler-utils
 fi
-cd ../volantino
-pdftk volantino_fronte.pdf volantino_retro.pdf cat output "$name_$version_$typeVersion.pdf"
+
+pdfunite  ../volantino/"$name"' '"$typeVersion"' FRONTE.pdf' ../volantino/"$name"' '"$typeVersion"' RETRO.pdf' ../volantino/"$name"' '"$version"' '"$typeVersion"'.pdf'
+echo "pdf merged correctly."
+echo "process successful."

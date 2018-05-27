52b2afb32765aacf9449f800eb3700e34ea3740a
mone27 <mone.massaro@gmail.com>
2018-05-27 13:44:58 +0200

    aggiunti parametri per versione nome tipo, con valori di default. aggiunta versione nel nome dell'output.

--- scripts/genera_pdf.sh
+++ scripts/genera_pdf.sh
@@ -3,12 +3,11 @@
 # and try to install all dependencies (tested on ubuntu and kubuntu)
 # now paths are hardcoded for firefox-vademecum repository
 # update manually, if it's necessary, the "name" variable
-
-name="Vademecum"
-echo "Version number (format: VV.VV): "
-read version
-echo "Version type {VG|VT}: "
-read typeVersion
+#usage version name typeVersion
+set -e
+version=${1-"2.0"}
+name=${2-"Vademecum"}
+typeVersion=${3-"VG"}
 
 if ! [ -x "$(command -v wkhtmltopdf)" ]; then
   if ! [ -x "$(command -v apt)" ]; then
@@ -19,13 +18,13 @@ if ! [ -x "$(command -v wkhtmltopdf)" ]; then
   sudo apt install wkhtmltopdf
 fi
 echo "Pdf files generating..."
-wkhtmltopdf -B 0 -T 0 -L 0 -R 0 ../html/volantino_fronte_"$typeVersion".html ../volantino/"$name"_"$typeVersion"_fronte.pdf
-wkhtmltopdf -B 0 -T 0 -L 0 -R 0 ../html/volantino_retro_"$typeVersion".html ../volantino/"$name"_"$typeVersion"_retro.pdf
+wkhtmltopdf -q -B 0 -T 0 -L 0 -R 0 ../html/volantino_fronte_"$typeVersion".html ../volantino/"$name"_"$version"_"$typeVersion"_fronte.pdf
+wkhtmltopdf -q -B 0 -T 0 -L 0 -R 0 ../html/volantino_retro_"$typeVersion".html ../volantino/"$name"_"$version"_"$typeVersion"_retro.pdf
 echo "Pdf files generated."
 
 echo "Png files generating..."
-wkhtmltoimage --crop-w 992 ../html/volantino_fronte_"$typeVersion".html ../volantino/"$name"_"$typeVersion"_fronte.png
-wkhtmltoimage --crop-w 992 ../html/volantino_retro_"$typeVersion".html ../volantino/"$name"_"$typeVersion"_retro.png
+wkhtmltoimage --crop-w 992 ../html/volantino_fronte_"$typeVersion".html ../volantino/"$name"_"$version"_"$typeVersion"_fronte.png
+wkhtmltoimage --crop-w 992 ../html/volantino_retro_"$typeVersion".html ../volantino/"$name"_"$version"_"$typeVersion"_retro.png
 echo "Png files generated."
 
 echo "Pdf files merging in a single pdf file..."
@@ -38,6 +37,6 @@ if ! [ -x "$(command -v pdfunite)" ]; then
   sudo apt install poppler-utils
 fi
 
-pdfunite  ../volantino/"$name"_"$typeVersion"_fronte.pdf ../volantino/"$name"_"$typeVersion"_retro.pdf ../volantino/"$name"_"$version"_"$typeVersion".pdf
+pdfunite  ../volantino/"$name"_"$version"_"$typeVersion"_fronte.pdf ../volantino/"$name"_"$version"_"$typeVersion"_retro.pdf ../volantino/"$name"_"$version"_"$typeVersion".pdf
 echo "Pdf merged correctly."
 echo "Process successful."

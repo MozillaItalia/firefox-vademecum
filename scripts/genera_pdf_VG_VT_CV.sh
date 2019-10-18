36664d5bdf52be3e1503bc7720bf4d56bf7dfb60
Daniele Scasciafratte <mte90net@gmail.com>
2019-10-18 14:39:42 +0200

    aggiornamento

--- scripts/genera_pdf_VG_VT_CV.sh
+++ scripts/genera_pdf_VG_VT_CV.sh
@@ -6,15 +6,11 @@ set -e
 version=${1-"2.0"}
 name=${2-"Vademecum"}
 
-# Barbatrucco per trovare dove eseguire lo script, in modo da
-# renderlo invocabile non solo da ./
-# ToDo: gestire PATH con spazi
-
 SCRIPT="$(readlink --canonicalize-existing "$0")"
 SCRIPTPATH="$(dirname "$SCRIPT")"
 cd "$SCRIPTPATH"
 
-# da aggiungere anche "CV", pe generare la versione Common Voice
+# ToDo: Aggiungere CV
 for i in "VG" "VT"
 do
     typeVersion=$i;
